7e29b7d6321e1d955d3e61a3f7d10de81353fcee
Igor Rocha <igorrocha.pf273@academico.ifsul.edu.br>
2023-10-16 11:27:33 -0300

    ajustes nas query

--- usecases/empresaUseCases.js
+++ usecases/empresaUseCases.js
@@ -26,10 +26,10 @@ const addEmpresaDB = async (body) => {
 
 const updateEmpresaDB = async (body) => {
     try {   
-        const {codigo, nome, razaosocial, cnpj ,sigla} = body;
-        const results = await pool.query(`UPDATE empresa SET  nome=$1, razaosocial=$2, cnpj=$3, sigla=$4
+        const {codigo, nome, cnpj ,sigla} = body;
+        const results = await pool.query(`UPDATE empresa SET  nome=$1,cnpj=$3, sigla=$4
         WHERE codigo=$5 returning codigo, nome, razaosocial, cnpj,sigla`, 
-        [nome, razaosocial, cnpj,sigla ,codigo]);        
+        [nome, cnpj,sigla ,codigo]);        
         if (results.rowCount == 0){
             throw `Nenhum registro encontrado com o código ${codigo} para ser alterado`;
         }
